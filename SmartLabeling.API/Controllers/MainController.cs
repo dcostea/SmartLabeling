4ef4dd0aafbb3b4581601239186ea27c984bb21a
Daniel Costea <daniel_costea@ymail.com>
2020-11-13 22:02:32 +0100

    Rename WeatherController to MainController.

---
+++ SmartLabeling.API/Controllers/MainController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SmartLabeling.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("GET triggered.");
+
+            return Ok();
+        }
+    }
+}
